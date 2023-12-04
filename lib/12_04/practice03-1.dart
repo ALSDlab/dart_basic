feba852bd4572a7f8c833e59a89aed4e4e90045d
ssd72 <sungdae.yi@gmail.com>
2023-12-04 05:35:35 +0100

    12월 4일 03-1 연습문제 2-2

--- lib/12_04/practice03-1.dart
+++ lib/12_04/practice03-1.dart
@@ -2,4 +2,19 @@
 // 2) 10명 학생의 시험 점수 : List
 // 3) 대한민국의 도시별 인구수(순서 상관없음): Map
 
+// 연습문제 2-2
 
+class Person {
+  String name;
+
+  Person({required this.name});
+}
+
+void main () {
+  final person1 = Person(name: '홍길동');
+  final person2 = Person(name: '한석봉');
+
+  List<String> people = [person1.name, person2.name];
+
+  print(people);
+}

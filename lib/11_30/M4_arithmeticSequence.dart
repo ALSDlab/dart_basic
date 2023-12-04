05904c1e5b4c6a98d3b1cd0ece5455a32f181777
ssd72 <sungdae.yi@gmail.com>
2023-12-04 07:44:42 +0100

    12월 4일 순한 맛 풀이

--- lib/11_30/M4_arithmeticSequence.dart
+++ lib/11_30/M4_arithmeticSequence.dart
@@ -2,11 +2,14 @@ void main(){
   int startNum = 5;
   int jumpNum = 10;
   int endNum = 10;
+  String result = '';
 
   print('등차수열 시작');
   for (int i=1;i<=endNum;i++){
-    print(startNum);
+    result += startNum.toString();
+    result += ' ';
     startNum += jumpNum;
   }
+  print(result);
   print('등차수열 끝');
 }

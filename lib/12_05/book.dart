2e754ffedc3cb8d38854d899a9eaafd5137396b8
ssd72 <sungdae.yi@gmail.com>
2023-12-05 07:26:57 +0100

    12월 5일 Dart05.추상클래스와 인터페이스 연습문제 12-1

---
+++ lib/12_05/book.dart
@@ -0,0 +1,6 @@
+import 'tangibleAsset.dart';
+
+class Book extends TangibleAsset {
+  String isbn;
+  Book(super.name, super.price, super.color, this.isbn);
+}
---
+++ lib/12_05/computer.dart
@@ -0,0 +1,9 @@
+import 'tangibleAsset.dart';
+
+class Computer extends TangibleAsset{
+  String makerName;
+
+  Computer(super.name, super.price, super.color,
+     this.makerName,
+  );
+}

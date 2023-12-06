9e74b147beda80655235146e9fe1bf68419954e8
ssd72 <sungdae.yi@gmail.com>
2023-12-06 06:23:15 +0100

    12월 5일 연습문제 풀이

---
+++ lib/12_06/polymorphism.dart
@@ -0,0 +1,51 @@
+abstract interface class X {
+  void a();
+}
+
+abstract class Y implements X {
+  void b();
+}
+
+class A extends Y {
+  @override
+  void a() {
+    print('Aa');
+  }
+
+  @override
+  void b() {
+    print('Ab');
+  }
+}
+
+class B extends Y {
+  @override
+  void a() {
+    print('Ba');
+  }
+
+  @override
+  void b() {
+    print('Bb');
+  }
+
+  void c() {
+    print("Bc");
+  }
+}
+
+void main(){
+  X obj = A();
+  obj.a();  //1. 정답
+
+  Y y1 = A();
+  Y y2 = B();
+  y1.a();    // Aa 출력
+  y2.a();    // Ba 출력
+
+  List<Y> values = [y1, y2];
+  for(Y val in values){
+    val.b();
+  }
+
+}

55e52e3053c667cdebccc21f33bf000eaf859bcf
ssd72 <sungdae.yi@gmail.com>
2023-12-08 09:07:46 +0100

    12월 8일 Dart 제네릭, 열거형 연습문제 2

--- lib/12_08/StrongBox.dart
+++ lib/12_08/StrongBox.dart
@@ -1,7 +1,41 @@
+enum KeyType {
+  padlock,
+  button,
+  dial,
+  finger,
+}
+
 class StrongBox<E> {
-  E? _property;
+  E? _key;
 
-  void put(E property) => _property = property;
+  void put(E key) => _key = key;
 
-  get property => _property;
+  KeyType? get() {
+    switch (_key) {
+      case 1024:
+        return KeyType.padlock;
+        break;
+      case 10000:
+        return KeyType.button;
+        break;
+      case 30000:
+        return KeyType.dial;
+        break;
+      case 1000000:
+        return KeyType.finger;
+        break;
+      default:
+        return null;
+        break;
+    }
+  }
+}
+
+void main() {
+  StrongBox<int> strongBox = StrongBox();
+
+  for (int i = 1; i <= 30000; i++) {
+    strongBox.put(i);
+    print('횟수: $i, 타입: ${strongBox.get()}');
+  }
 }

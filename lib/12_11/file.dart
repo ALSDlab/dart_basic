2420bb42079e5a9d08335495e9373b95bd6f9d2d
ssd72 <sungdae.yi@gmail.com>
2023-12-11 03:28:39 +0100

    12월 11일 Dart 파일읽기 쓰기

---
+++ lib/12_11/file.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  // Open
+  final file = File('save.txt');
+
+  // 파일을 읽거나 쓰고 닫는다.
+  // file.writeAsStringSync('Hello World!!');
+  try {
+    final text = file.readAsStringSync();
+    print(text);
+  } catch (e) {
+    print('파일이 없습니다. : $e');
+  }
+}

2ec7c9b00a052d4dbb7b2e39c506050b14e9848d
ssd72 <sungdae.yi@gmail.com>
2023-12-11 06:57:42 +0100

    12월 11일 Dart 파일조작 연습문제 1

---
+++ lib/12_11/practice2_file.dart
@@ -0,0 +1,17 @@
+// 1. 파일을 복사하는 함수를 작성하시오
+//    원본 파일 경로와 복사할 파일 경로는 프로그램 실행 시 파라미터로 전달되는 것으로 하고, 버퍼링이나 에러 처리는 하지 않는다.
+
+import 'dart:io';
+
+void copy(String source, String target) {
+  final originalFile = File('lib/12_11/$source');
+  final copyFile = File('lib/12_11/$target');
+  final text = originalFile.readAsStringSync();
+  copyFile.writeAsStringSync(text);
+}
+
+void main() {
+  final sourceFile = 'test_file.txt';
+  final targetFile = 'test_target.txt';
+  copy(sourceFile, targetFile);
+}

fac41fecf7ddf315eff7b03246152923246c118c
ssd72 <sungdae.yi@gmail.com>
2023-12-11 12:17:14 +0100

    12월 11일 Dart 예외 연습문제 1, 2

--- lib/12_11/practice1_exception.dart
+++ lib/12_11/practice1_exception.dart
@@ -2,13 +2,16 @@ void main() {
   // 1. 다음과 같은 프로그램을 작성, 실행 후 runtime error 를 발생하시오.
   // 2. 1에서 작성한 코드를 수정하여, try-catch()문을 사용하여 예외처리를 하시오.
   //    예외처리는 다음의 처리를 수행하시오.
-  final numString = '10.5';
+  final String numString = '10.5';
+  print(parse(numString));
+}
 
+int parse(String string) {
+  int num;
   try {
-    int num = int.parse(numString);
-    print(num);
-  } on FormatException {
-    int defaultNum = 0;
-    print(defaultNum);
+    num = int.parse(string);
+  } catch (e) {
+    num = 0;
   }
+  return num;
 }

1f4df02ffd42ae5ff5bb27efe6ca975ec6630447
ssd72 <sungdae.yi@gmail.com>
2023-12-13 11:49:11 +0100

    12월 13일 Dart 비동기 연습문제 1 파일 읽기, 쓰기 함수 분리

--- lib/12_13/practice1_async.dart
+++ lib/12_13/practice1_async.dart
@@ -1,30 +1,41 @@
 import 'dart:io';
 
-Future<void> readAndWriteFileAsync(File source, File target) async {
-  String contents;
+Future<String> readFileAsync(File source) async {
+  String contents = '';
   try {
     contents = await source.readAsString();
     print('파일 내용: $contents');
-    await target.writeAsString(findSukbong(contents.toString()));
-    print('파일 수정 성공');
   } catch (e) {
     print('파일을 읽는 도중 오류 발생: $e');
   }
+  return contents;
 }
 
-String findSukbong(String source) {
+Future<void> writeFileAsync(String source, File target) async {
+  try {
+    await target.writeAsString(source);
+    print('파일 수정 성공');
+  } catch (e) {
+    print('파일을 쓰는 도중 오류 발생: $e');
+  }
+}
+
+String findWord(String source) {
   String replacedText;
   if (source.contains('한석봉')) {
     replacedText = source.replaceAll('한석봉', '김석봉');
+    print('새로운 내용: $replacedText');
   } else {
     replacedText = source;
   }
   return replacedText;
 }
 
-void main() {
+void main() async {
   File file = File('lib/12_13/sample.csv');
   File copyFile = File('lib/12_13/sample_copy.csv');
 
-  readAndWriteFileAsync(file, copyFile);
+  String fileData = await readFileAsync(file);
+  String newText = findWord(fileData);
+  await writeFileAsync(newText, copyFile);
 }

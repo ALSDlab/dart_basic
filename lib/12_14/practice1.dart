3a83af1dbd30cee9e5b31ed1c2b9b30c6d3ad890
ssd72 <sungdae.yi@gmail.com>
2023-12-14 09:29:40 +0100

    12월 14일 Dart 네트워크 통신 연습문제 1 - 문제내용 주석으로 적었음

--- lib/12_14/practice1.dart
+++ lib/12_14/practice1.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 import 'practice1_movie_infos.dart';
 import 'package:http/http.dart' as http;
 
+
+// 1. 영화정보를 가져와서 모델에 담고 json 형태로 출력하기
+
 Future<MovieInfos> getMovies(int page) async {
   // 요청
   final response = await http.get(Uri.parse(
